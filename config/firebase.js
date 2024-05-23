e2054b765ae52fee644ffe5b562e222cceeed420
TUAJEEDZ <153889408+tjzwave@users.noreply.github.com>
2024-05-23 22:20:16 +0700

    อันใหม่ by TUAJEEDZ

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "@firebase/app";
 import { getAnalytics } from "@firebase/analytics";
 import { getAuth } from "@firebase/auth";
@@ -7,7 +6,11 @@ import { getFirestore } from "firebase/firestore";
 import firebase from "firebase/compat/app";
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore'
-
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/compat/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,6 +24,8 @@ const firebaseConfig = {
   measurementId: "G-2BCLC0BSF2"
 };
 
+
+
 if (!firebase.apps.length){
   firebase.initializeApp(firebaseConfig)
 }
@@ -29,6 +34,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const firestore = firebase.firestore();
 
-
-export {firebase, app, analytics, auth, db };
+export {firebase, app, analytics, auth, db,firestore,  };
