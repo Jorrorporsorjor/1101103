ff798731d4c571816d04827e086a1eebcfc4f9bb
Pluem-piyapong <k.55.zaza22@gmail.com>
2024-05-28 21:30:21 +0700

    Update MA.js
    ui เสร็จ

--- screens/MA.js
+++ screens/MA.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { firebase } from '../config/firebase';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Alert } from 'react-native';
+
 
 const MAScreen = ({ navigation }) => {
   const [users, setUsers] = useState([]);
@@ -32,6 +35,22 @@ const MAScreen = ({ navigation }) => {
     );
     return () => unsubscribe();
   }, []);
+  
+  const showAlert = (description) => {
+    Alert.alert(
+      "คำอธิบายเพิ่มเติม",
+      description,
+      [{ text: "ตกลง", style: 'cancel', textStyle: styles.alertButton }],
+      { cancelable: false,
+        style: 'default',
+        titleStyle: { color: 'red' }, // เปลี่ยนสีของหัวข้อ
+        messageStyle: { color: 'blue' } // เปลี่ยนสีของข้อความเนื้อหา
+      }
+    );
+  };
+  
+
+  
 
   if (loading) {
     return <Text>Loading...</Text>;
@@ -39,93 +58,219 @@ const MAScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.section1}>
-        <Text style={[styles.text, { textAlign: 'center' }]}>MOVIE & ANIMATION</Text>
-        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-          <AntDesign name="arrowleft" size={24} color="black" />
-          <Text style={styles.backButtonText}>Back</Text>
-        </TouchableOpacity>
-      </View>
+      <LinearGradient
+                colors={['#8678c1', '#7164b6']}
+                style={styles.section1}
+                start={{ x: 0, y: 0.5 }}
+                end={{ x: 1, y: 0.5 }}
+            >
+                <Text style={[styles.text, { textAlign: 'center' }]}>MOVIE & ANIMATION</Text>
+                <Text style={[styles.text2, { textAlign: 'center' }]}>ภาพยนตร์ และแอนิเมชัน</Text>
+            </LinearGradient>
       <ScrollView contentContainerStyle={styles.scrollViewContainer}>
         {users.map((item) => (
           <View key={item.id} style={styles.itemContainer}>
+            <View style={styles.scrollViewContainer2}></View>           
             {/* Display the image */}
             <Image source={{ uri: item.image }} style={styles.profileImage} />
             <View style={styles.textContainer}>
-              <Text style={styles.itemText}>Movie Name: {item.name}</Text>
-              <Text style={styles.itemText}>Author: {item.Author}</Text>
-              <Text style={styles.itemText}>Description: {item.description}</Text>
+              <Text style={styles.textMovie}>Name: {item.name}</Text>
+                <Text style={styles.textMovie2}>Author: {item.Author}</Text>
+              <TouchableOpacity onPress={() => showAlert(item.description)}>
+              <Text style={styles.textMovie3}>Description {item.description} </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => showAlert(item.description)}>
+              <Text style={styles.textMovie4}>เพิ่มเติม</Text>
+              </TouchableOpacity>
             </View>
           </View>
         ))}
+        <View></View>
       </ScrollView>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <AntDesign name="arrowleft" size={24} color="#e6e6ee" />
+          <Text style={styles.backButtonText}>Back</Text>
+        </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
-  },
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: '#dcd9f4',
+    shadowColor: '#000', // สีของเงา
+    shadowOffset: { width: 0, height: 2 }, // การเลื่อนของเงา
+    shadowOpacity: 1, // ความทึบของเงา
+    shadowRadius: 2, // รัศมีของเงา
+    elevation: 5, // การยกระดับของเงา (สำหรับ Android)
+  }, 
   section1: {
-    height: '17%',
-    backgroundColor: '#FFAB7A',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative', // Allow positioning of back button
+    height: '19%',
+        backgroundColor: '#f4ebdc',
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
+        borderBottomRightRadius: 60,
+        borderBottomLeftRadius: 60,
+        shadowColor: '#000', // สีของเงา
+        shadowOffset: { width: 0, height: 2 }, // การเลื่อนของเงา
+        shadowOpacity: 1, // ความทึบของเงา
+        shadowRadius: 2, // รัศมีของเงา
+        elevation: 5, // การยกระดับของเงา (สำหรับ Android)
   },
-  text: {
-    fontSize: 30,
-    color: 'black',
+  text2: {
+    fontSize: 25, // ปรับขนาดตัวอักษร
+    color: 'white',
     fontWeight: 'bold',
-    lineHeight: 36,
-    marginTop: 20,
-  },
+    lineHeight: 30, // ปรับระยะห่างระหว่างบรรทัด
+    marginTop: 0, // เว้นระยะห่างด้านบน
+    textShadowColor: 'rgba(0, 0, 0, 0.75)', // สีของเงา
+    textShadowOffset: { width: -1, height: 1 }, // ตำแหน่งของเงา
+    textShadowRadius: 10, // รัศมีของเงา
+},
+text: {
+    fontSize: 20, // ปรับขนาดตัวอักษร
+    color: 'white',
+    fontWeight: 'bold',
+    lineHeight: 30, // ปรับระยะห่างระหว่างบรรทัด
+    marginTop: 50, // เว้นระยะห่างด้านบน
+    marginBottom: 0, // เว้นระยะห่างด้านล่าง
+    textShadowColor: 'rgba(0, 0, 0, 0.75)', // สีของเงา
+    textShadowOffset: { width: -1, height: 1 }, // ตำแหน่งของเงา
+    textShadowRadius: 10, // รัศมีของเงา
+    
+},
   scrollViewContainer: {
     flexGrow: 1,
-    backgroundColor: '#9379C2',
+    backgroundColor: '#dcd9f4',
     alignItems: 'center',
     paddingTop: 20,
+    
+  },
+  scrollViewContainer2: {
+    height: 70,
+    width: '107%',
+    backgroundColor: '#eeeef4',
+    paddingTop: 20,
+    top: 20,
+    borderTopLeftRadius: 20,
+        borderTopRightRadius: 10,
+         borderBottomRightRadius: 10,
+           borderBottomLeftRadius: 10,
+             
   },
   itemContainer: {
-    flexDirection: 'row', // Align image and text horizontally
-    alignItems: 'flex-start', // Align items at the top vertically
-    width: '80%',
-    backgroundColor: '#FFFFFF',
-    borderRadius: 10,
+    height: 220,
+    width: '82%',
+    backgroundColor: '#dcd9f4',
+    borderTopLeftRadius: 20,
+        borderTopRightRadius: 5,
+         borderBottomRightRadius: 5,
+           borderBottomLeftRadius: 5,
     padding: 10,
-    marginVertical: 10,
-  },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 10,
-  },
-  textContainer: {
-    flex: 1, // Allow text to take up remaining space
-  },
-  itemText: {
-    fontSize: 16,
-    marginBottom: 5,
+    marginVertical: 20,
+    top: -10,
+    marginTop: -10,
+   
+    
+    
   },
   backButton: {
     flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#ddd',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 30,
-    position: 'absolute',
-    top: 40,
-    left: 10,
+        alignItems: 'center',
+        backgroundColor: '#8678c1',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 30,
+        position: 'absolute',
+        top: 720,
+        left: 250,
+        shadowColor: '#000', // สีของเงา
+        shadowOffset: { width: 0, height: 1 }, // การเลื่อนของเงา
+        shadowOpacity: 0.8, // ความทึบของเงา
+        shadowRadius: 1, // รัศมีของเงา
+        elevation: 4, // การยกระดับของเงา (สำหรับ Android)
   },
   backButtonText: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: 'black',
+    color: '#e6e6ee',
     marginLeft: 10,
   },
+  profileImage: {
+    width: 120,
+    height: 200,
+    marginRight: 10,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    borderBottomLeftRadius: 5,
+    left: -20,
+    top: -55,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 1 }, // Shadow offset
+    shadowOpacity: 0.8, // Shadow opacity
+    shadowRadius: 1, // Shadow radius
+    elevation: 5, // Elevation for Android shadow
+       
+  },
+  textMovie: {
+    width: '80%',
+    fontSize: 18, // ปรับขนาดตัวอักษร
+    color: '#5B4F92',
+    fontWeight: 'bold',
+    lineHeight: 20, // ปรับระยะห่างระหว่างบรรทัด
+    marginTop: 10, // เว้นระยะห่างด้านบน
+    marginBottom: 0, // เว้นระยะห่างด้านล่าง
+    top: -250,
+    left: 110,
+},
+textMovie2: {
+  width: '80%',
+  fontSize: 12, // ปรับขนาดตัวอักษร
+  color: '#8678c1',
+  fontWeight: 'bold',
+  lineHeight: 13, // ปรับระยะห่างระหว่างบรรทัด
+  marginTop: 0, // เว้นระยะห่างด้านบน
+  marginBottom: 0, // เว้นระยะห่างด้านล่าง
+  top: -250,
+    left: 110,
+},
+textMovie3: {
+  height: 50,
+  width: '60%',
+  fontSize: 9, // ปรับขนาดตัวอักษร
+  color: '#5B4F92',
+  fontWeight: 'bold',
+  lineHeight: 13, // ปรับระยะห่างระหว่างบรรทัด
+  marginTop: 40, // เว้นระยะห่างด้านบน
+  marginBottom: 0, // เว้นระยะห่างด้านล่าง
+  top: -250,
+    left: 110,
+},
+textMovie4: {
+  height: 50,
+  width: '60%',
+  fontSize: 9, // ปรับขนาดตัวอักษร
+  color: '#5B4F92',
+  fontWeight: 'bold',
+  lineHeight: 13, // ปรับระยะห่างระหว่างบรรทัด
+  marginTop: 0, // เว้นระยะห่างด้านบน
+  marginBottom: 0, // เว้นระยะห่างด้านล่าง
+  top: -250,
+    left: 110,
+},
+descriptionButton: {
+  width: '70%',
+    backgroundColor: '#2ecc71',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    top: -190,
+    left: 110,
+},
 });
 
 export default MAScreen;
