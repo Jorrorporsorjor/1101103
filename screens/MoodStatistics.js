d5fb44b03f1acb76cf13be42464d68864b01a395
Patyz-Hack <126221933+Nongteez@users.noreply.github.com>
2024-05-28 23:51:25 +0700

    Update MoodStatistics.js
    anima

--- screens/MoodStatistics.js
+++ screens/MoodStatistics.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Image, Animated } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
 
 const moodIcons = {
-  1: { image: require('../pic/emo_awful.png'), name: 'Awful', color: '#fe6667' }, 
-  2: { image: require('../pic/emo_bad.png'), name: 'Bad', color: '#ffa648' },   
-  3: { image: require('../pic/emo_meh.png'), name: 'Meh', color: '#71b5e6' },    
-  4: { image: require('../pic/emo_good.png'), name: 'Good', color: '#a4d653' },  
-  5: { image: require('../pic/emo_great.png'), name: 'Great', color: '#41c7a4' }, 
+  1: { image: require('../pic/emo_awful.png'), name: 'Awful', color: '#fe6667' },
+  2: { image: require('../pic/emo_bad.png'), name: 'Bad', color: '#ffa648' },
+  3: { image: require('../pic/emo_meh.png'), name: 'Meh', color: '#71b5e6' },
+  4: { image: require('../pic/emo_good.png'), name: 'Good', color: '#a4d653' },
+  5: { image: require('../pic/emo_great.png'), name: 'Great', color: '#41c7a4' },
 };
 
 const MoodStatistics = ({ moodData }) => {
@@ -26,7 +26,7 @@ const MoodStatistics = ({ moodData }) => {
     <View style={styles.container}>
       {sortedMoods.map(([mood, count]) => (
         <View key={mood} style={styles.moodRow}>
-          <Image source={moodIcons[mood].image} style={styles.moodImage} />
+          <ShakingImage source={moodIcons[mood].image} />
           <Text style={styles.moodText}>{moodIcons[mood].name}</Text>
           <View style={styles.progressContainer}>
             <ProgressBar
@@ -42,10 +42,41 @@ const MoodStatistics = ({ moodData }) => {
   );
 };
 
+const ShakingImage = ({ source }) => {
+  const shakeAnimation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(shakeAnimation, { toValue: 1, duration: 800, useNativeDriver: true }),
+        Animated.timing(shakeAnimation, { toValue: -1, duration: 200, useNativeDriver: true }),
+        Animated.timing(shakeAnimation, { toValue: 0, duration: 800, useNativeDriver: true }),
+      ])
+    ).start();
+  }, [shakeAnimation]);
+
+  const shakeStyle = {
+    transform: [
+      {
+        translateX: shakeAnimation.interpolate({
+          inputRange: [-1, 1],
+          outputRange: [-5, 5],
+        }),
+      },
+    ],
+  };
+
+  return <Animated.Image source={source} style={[styles.moodImage, shakeStyle]} />;
+};
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
-    paddingHorizontal: 10,
+    marginTop: 1,
+    paddingHorizontal: 20,
+    backgroundColor: '#f8f8f8',
+    borderRadius: 10,
+    padding: 5,
+    elevation: 3,
   },
   moodRow: {
     flexDirection: 'row',
@@ -53,17 +84,21 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
   moodImage: {
-    width: 30,
-    height: 30,
-    marginRight: 10,
+    marginTop: 1,
+    width: 40,
+    height: 40,
+    marginRight: 5,
   },
   moodText: {
+    marginTop: 1,
     flex: 1,
     fontSize: 16,
+    color: '#0B0428',
+    fontWeight: 'bold',
   },
   progressContainer: {
     flex: 3,
-    marginRight: 10,
+    marginRight: 15,
   },
   progressBar: {
     height: 10,
@@ -72,7 +107,8 @@ const styles = StyleSheet.create({
   percentageText: {
     flex: 1,
     textAlign: 'right',
-    fontSize: 16,
+    fontSize: 15,
+    color: '#666',
   },
 });
 
